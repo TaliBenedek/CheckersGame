0bf274b2a11bdfb610ec5ca970faf76bc0a616ba
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-11-08 18:46:57 -0500

    Added gitignore and removed files

--- CheckersGame/Form1.Designer.cs
+++ CheckersGame/Form1.Designer.cs
@@ -47,22 +47,23 @@ namespace CheckersGame
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(99, 29);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(74, 23);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(667, 625);
+            this.panel1.Size = new System.Drawing.Size(500, 500);
             this.panel1.TabIndex = 0;
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Easy",
             "Medium",
             "Hard"});
-            this.comboBox1.Location = new System.Drawing.Point(869, 201);
+            this.comboBox1.Location = new System.Drawing.Point(652, 161);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 33);
+            this.comboBox1.Size = new System.Drawing.Size(198, 28);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -70,9 +71,10 @@ namespace CheckersGame
             // 
             this.redButton.AutoSize = true;
             this.redButton.Checked = true;
-            this.redButton.Location = new System.Drawing.Point(3, 31);
+            this.redButton.Location = new System.Drawing.Point(2, 25);
+            this.redButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.redButton.Name = "redButton";
-            this.redButton.Size = new System.Drawing.Size(74, 29);
+            this.redButton.Size = new System.Drawing.Size(57, 24);
             this.redButton.TabIndex = 2;
             this.redButton.TabStop = true;
             this.redButton.Text = "red";
@@ -81,9 +83,10 @@ namespace CheckersGame
             // whiteButton
             // 
             this.whiteButton.AutoSize = true;
-            this.whiteButton.Location = new System.Drawing.Point(94, 31);
+            this.whiteButton.Location = new System.Drawing.Point(70, 25);
+            this.whiteButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.whiteButton.Name = "whiteButton";
-            this.whiteButton.Size = new System.Drawing.Size(93, 29);
+            this.whiteButton.Size = new System.Drawing.Size(71, 24);
             this.whiteButton.TabIndex = 3;
             this.whiteButton.Text = "white";
             this.whiteButton.UseVisualStyleBackColor = true;
@@ -93,9 +96,10 @@ namespace CheckersGame
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(898, 118);
+            this.label1.Location = new System.Drawing.Point(674, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 51);
+            this.label1.Size = new System.Drawing.Size(161, 38);
             this.label1.TabIndex = 4;
             this.label1.Text = "Difficulty:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -104,9 +108,10 @@ namespace CheckersGame
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(873, 330);
+            this.label2.Location = new System.Drawing.Point(655, 264);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(257, 37);
+            this.label2.Size = new System.Drawing.Size(197, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Choose a color:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -115,17 +120,19 @@ namespace CheckersGame
             // 
             this.panel2.Controls.Add(this.whiteButton);
             this.panel2.Controls.Add(this.redButton);
-            this.panel2.Location = new System.Drawing.Point(892, 370);
+            this.panel2.Location = new System.Drawing.Point(669, 296);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.Size = new System.Drawing.Size(150, 80);
             this.panel2.TabIndex = 7;
             // 
             // startButton
             // 
             this.startButton.Font = new System.Drawing.Font("Tahoma", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.startButton.Location = new System.Drawing.Point(926, 529);
+            this.startButton.Location = new System.Drawing.Point(694, 423);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(135, 67);
+            this.startButton.Size = new System.Drawing.Size(101, 54);
             this.startButton.TabIndex = 8;
             this.startButton.Text = "START";
             this.startButton.UseVisualStyleBackColor = true;
@@ -134,9 +141,10 @@ namespace CheckersGame
             // whitePiece
             // 
             this.whitePiece.Image = ((System.Drawing.Image)(resources.GetObject("whitePiece.Image")));
-            this.whitePiece.Location = new System.Drawing.Point(799, 623);
+            this.whitePiece.Location = new System.Drawing.Point(599, 498);
+            this.whitePiece.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.whitePiece.Name = "whitePiece";
-            this.whitePiece.Size = new System.Drawing.Size(97, 46);
+            this.whitePiece.Size = new System.Drawing.Size(73, 37);
             this.whitePiece.TabIndex = 9;
             this.whitePiece.TabStop = false;
             this.whitePiece.Visible = false;
@@ -144,18 +152,19 @@ namespace CheckersGame
             // redPiece
             // 
             this.redPiece.Image = ((System.Drawing.Image)(resources.GetObject("redPiece.Image")));
-            this.redPiece.Location = new System.Drawing.Point(1008, 623);
+            this.redPiece.Location = new System.Drawing.Point(756, 498);
+            this.redPiece.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.redPiece.Name = "redPiece";
-            this.redPiece.Size = new System.Drawing.Size(100, 50);
+            this.redPiece.Size = new System.Drawing.Size(75, 40);
             this.redPiece.TabIndex = 10;
             this.redPiece.TabStop = false;
             this.redPiece.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1230, 681);
+            this.ClientSize = new System.Drawing.Size(922, 545);
             this.Controls.Add(this.redPiece);
             this.Controls.Add(this.whitePiece);
             this.Controls.Add(this.startButton);
@@ -164,9 +173,8 @@ namespace CheckersGame
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Checkers";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
