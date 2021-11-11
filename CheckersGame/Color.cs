9f6aafd874e71365bb295a34299a486fc805febf
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-11-10 21:51:32 -0500

    Added player and color enums

---
+++ CheckersGame/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame
+{
+    class Color
+    {
+        public enum PieceColor
+        {
+            Red,
+            White
+        }
+    }
+}
