05329b96b6ea53f62fd3d804926db0318a356290
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-11-10 22:00:05 -0500

    Removed outdated code

--- CheckersGame/Board.cs
+++ CheckersGame/Board.cs
@@ -8,11 +8,6 @@ namespace CheckersGame
 {
     class Board
     {
-        public enum Player
-        {
-            Computer,
-            Human
-        }
         private const int COLUMNS = 8;
         private const int ROWS = 8;
         private Square[,] squares = new Square[COLUMNS, ROWS];
@@ -35,23 +30,27 @@ namespace CheckersGame
         {
             for (int column = 0; column < COLUMNS; column++)
             {
+                //computer rows
                 int firstRow = 0;
                 int secondRow = 1;
                 int thirdRow = 2;
+
+                //user rows
                 int sixthRow = 5;
                 int seventhRow = 6;
                 int eighthRow = 7;
+
                 if (column % 2 == 0)
                 {
-                    squares[column, secondRow].Piece = (new RegularPiece(new Location(secondRow, column), computerColor));
-                    squares[column, sixthRow].Piece = (new RegularPiece(new Location(sixthRow, column), playerColor));
-                    squares[column, eighthRow].Piece = (new RegularPiece(new Location(eighthRow, column), playerColor));
+                    squares[column, secondRow].Piece = (new RegularPiece(new Location(secondRow, column), computerColor, PlayerType.Player.Computer));
+                    squares[column, sixthRow].Piece = (new RegularPiece(new Location(sixthRow, column), playerColor, PlayerType.Player.User));
+                    squares[column, eighthRow].Piece = (new RegularPiece(new Location(eighthRow, column), playerColor, PlayerType.Player.User));
                 }
                 else
                 {
-                    squares[column, firstRow].Piece = (new RegularPiece(new Location(firstRow, column), computerColor));
-                    squares[column, thirdRow].Piece = (new RegularPiece(new Location(thirdRow, column), computerColor));
-                    squares[column, seventhRow].Piece = (new RegularPiece(new Location(seventhRow, column), playerColor));
+                    squares[column, firstRow].Piece = (new RegularPiece(new Location(firstRow, column), computerColor, PlayerType.Player.Computer));
+                    squares[column, thirdRow].Piece = (new RegularPiece(new Location(thirdRow, column), computerColor, PlayerType.Player.Computer));
+                    squares[column, seventhRow].Piece = (new RegularPiece(new Location(seventhRow, column), playerColor, PlayerType.Player.User));
                 }
             }
         }
